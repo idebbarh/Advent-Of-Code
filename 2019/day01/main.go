22bfff7996fa7f4d57d656ab5fbcb47699def8de
ismail debbarh <usamdh07@gmail.com>
2023-10-14 16:31:38 +0100

    year 2019 day 01 in golang

---
+++ 2019/day01/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func getLines(s string) []int {
+	stringLines := strings.Split(s, "\n")
+	var numberLines []int
+
+	for _, line := range stringLines {
+		if len(line) == 0 {
+			continue
+		}
+		n, err := strconv.Atoi(line)
+		if err != nil {
+			fmt.Printf("ERROR: could not convert string %+v to int", line)
+			os.Exit(1)
+		}
+
+		numberLines = append(numberLines, n)
+	}
+
+	return numberLines
+}
+
+func openFile(filePath string) string {
+	fileData, err := os.ReadFile(filePath)
+	if err != nil {
+		fmt.Printf("ERROR: could not read file %+v: %+v", filePath, err)
+		os.Exit(1)
+	}
+	return string(fileData)
+}
+
+func main() {
+	fileData := openFile("./real-input.txt")
+	lines := getLines(fileData)
+	fuelRequired := 0
+
+	for _, line := range lines {
+		fuelRequired += (line / 3) - 2
+	}
+
+	println(fuelRequired)
+}
