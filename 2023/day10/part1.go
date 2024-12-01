ab3a2e183686ca7f0339eddd24b98f4b1c80ee81
ismail debbarh <usamdh07@gmail.com>
2024-12-01 14:39:17 +0100

    feat: 2023 day10 part 2 golang

--- 2023/day10/part1.go
+++ 2023/day10/part1.go
@@ -1,36 +1,5 @@
 package main
 
-import (
-	"strings"
-)
-
-type Node struct {
-	row  int
-	col  int
-	pipe string
-}
-
-type Nodes = []Node
-
-type Graph = map[Node]Nodes
-
-type (
-	Col  = string
-	Row  = []Col
-	Grid = []Row
-)
-
-type Pipe = string
-
-const (
-	NORTH_SOUTH Pipe = "|"
-	EAST_WEST   Pipe = "-"
-	NORTH_EAST  Pipe = "L"
-	NORTH_WEST  Pipe = "J"
-	SOUTH_WEST  Pipe = "7"
-	SOUTH_EAST  Pipe = "F"
-)
-
 func graph_traverse(cur_node Node, vis map[Node]bool, graph Graph, total int, steps []int) []int {
 	if cur_node.pipe == "S" {
 		return steps
@@ -61,117 +30,10 @@ func graph_traverse(cur_node Node, vis map[Node]bool, graph Graph, total int, st
 	return res
 }
 
-func node_connected_neighbrs(node Node, grid Grid) Nodes {
-	neibrs := Nodes{}
-	M := len(grid)
-	N := len(grid[0])
-	nr := node.row
-	nc := node.col
-	//    north
-	// west-|-east
-	//    south
-
-	// | is a vertical pipe connecting north and south.
-	// - is a horizontal pipe connecting east and west.
-	// L is a 90-degree bend connecting north and east.
-	// J is a 90-degree bend connecting north and west.
-	// 7 is a 90-degree bend connecting south and west.
-	// F is a 90-degree bend connecting south and east.
-	// . is ground; there is no pipe in this tile.
-	// S is the starting position of the animal; there is a pipe on this tile, but your sketch doesn't show what shape the pipe has.
-
-	switch node.pipe {
-	case NORTH_SOUTH:
-		if nr-1 >= 0 && grid[nr-1][nc] != "." {
-			neibrs = append(neibrs, Node{row: nr - 1, col: nc, pipe: grid[nr-1][nc]})
-		}
-		if nr+1 < M && grid[nr+1][nc] != "." {
-			neibrs = append(neibrs, Node{row: nr + 1, col: nc, pipe: grid[nr+1][nc]})
-		}
-	case EAST_WEST:
-		if nc-1 >= 0 && grid[nr][nc-1] != "." {
-			neibrs = append(neibrs, Node{row: nr, col: nc - 1, pipe: grid[nr][nc-1]})
-		}
-		if nc+1 < N && grid[nr][nc+1] != "." {
-			neibrs = append(neibrs, Node{row: nr, col: nc + 1, pipe: grid[nr][nc+1]})
-		}
-	case NORTH_EAST:
-		if nr-1 >= 0 && grid[nr-1][nc] != "." {
-			neibrs = append(neibrs, Node{row: nr - 1, col: nc, pipe: grid[nr-1][nc]})
-		}
-
-		if nc+1 < N && grid[nr][nc+1] != "." {
-			neibrs = append(neibrs, Node{row: nr, col: nc + 1, pipe: grid[nr][nc+1]})
-		}
-	case NORTH_WEST:
-		if nr-1 >= 0 && grid[nr-1][nc] != "." {
-			neibrs = append(neibrs, Node{row: nr - 1, col: nc, pipe: grid[nr-1][nc]})
-		}
-		if nc-1 >= 0 && grid[nr][nc-1] != "." {
-			neibrs = append(neibrs, Node{row: nr, col: nc - 1, pipe: grid[nr][nc-1]})
-		}
-	case SOUTH_WEST:
-		if nr+1 < M && grid[nr+1][nc] != "." {
-			neibrs = append(neibrs, Node{row: nr + 1, col: nc, pipe: grid[nr+1][nc]})
-		}
-		if nc-1 >= 0 && grid[nr][nc-1] != "." {
-			neibrs = append(neibrs, Node{row: nr, col: nc - 1, pipe: grid[nr][nc-1]})
-		}
-	case SOUTH_EAST:
-		if nr+1 < M && grid[nr+1][nc] != "." {
-			neibrs = append(neibrs, Node{row: nr + 1, col: nc, pipe: grid[nr+1][nc]})
-		}
-		if nc+1 < N && grid[nr][nc+1] != "." {
-			neibrs = append(neibrs, Node{row: nr, col: nc + 1, pipe: grid[nr][nc+1]})
-		}
-
-	}
-	return neibrs
-}
-
 func part1(input string) int {
 	output := 0
-	lines := strings.Split(input, "\n")
-	grid := Grid{}
-	start_node := Node{row: 0, col: 0, pipe: "S"}
-	for _, line := range lines {
-		if len(line) == 0 {
-			continue
-		}
-		row := strings.Split(line, "")
-		grid = append(grid, row)
-	}
-
-	M := len(grid)
-	N := len(grid[0])
-	graph := Graph{}
-
-	for i := 0; i < M; i++ {
-		for j := 0; j < N; j++ {
-			pipe := grid[i][j]
-			if pipe == "." || pipe == "S" {
-				if pipe == "S" {
-					start_node.row = i
-					start_node.col = j
-				}
-				continue
-			}
-
-			node := Node{row: i, col: j, pipe: pipe}
-			graph[node] = node_connected_neighbrs(node, grid)
-		}
-	}
-	start_node_neighbrs := Nodes{}
-
-	for node, neibrs := range graph {
-		for _, n := range neibrs {
-			if n.pipe == "S" {
-				start_node_neighbrs = append(start_node_neighbrs, node)
-			}
-		}
-	}
-
-	graph[start_node] = start_node_neighbrs
+	graph, start_node, _ := get_graph(input)
+	start_node_neighbrs := graph[start_node]
 
 	cicle := graph_traverse(start_node_neighbrs[0], map[Node]bool{}, graph, 1, []int{})
 	rev_cicle := []int{}
--- 2023/day10/part2.go
+++ 2023/day10/part2.go
@@ -1,6 +1,98 @@
 package main
 
+import (
+	"fmt"
+	"slices"
+)
+
+type Point struct {
+	x int
+	y int
+}
+
+type Edge struct {
+	p1 Point
+	p2 Point
+}
+
+type Edges = []Edge
+
+func is_inside(node Node, grid Grid, polygon_nodes Nodes) bool {
+	inter_count := 0
+	for j := node.col + 1; j < len(grid[0]); j++ {
+		inter_node := Node{row: node.row, col: j, pipe: grid[node.row][j]}
+		if slices.Contains(polygon_nodes, inter_node) {
+			inter_count++
+			if j < len(grid[0])-1 {
+				is_next_same := slices.Contains(polygon_nodes, Node{row: node.row, col: j + 1, pipe: grid[node.row][j+1]})
+				if is_next_same {
+					inter_count++
+				}
+				for j < len(grid[0]) && is_next_same {
+					is_next_same = slices.Contains(polygon_nodes, Node{row: node.row, col: j, pipe: grid[node.row][j]})
+					j += 1
+				}
+			}
+		}
+	}
+
+	return inter_count%2 != 0
+}
+
+func get_polygon(cur_node Node, graph Graph, vis map[Node]bool, nodes Nodes) Nodes {
+	if _, ok := vis[cur_node]; ok {
+		if cur_node.pipe == "S" {
+			return nodes
+		}
+		return Nodes{}
+	}
+
+	vis[cur_node] = true
+	res := []Node{}
+	neighbrs := graph[cur_node]
+
+	for _, nei := range neighbrs {
+		cur_res := get_polygon(nei, graph, vis, append(nodes, cur_node))
+
+		if len(res) < len(cur_res) {
+			res = cur_res
+		}
+
+	}
+	return res
+}
+
 func part2(input string) int {
 	output := 0
+	encloses := Nodes{}
+	graph, start_node, grid := get_graph(input)
+	polygon_nodes := get_polygon(start_node, graph, map[Node]bool{}, Nodes{})
+	polygon_edges := Edges{}
+
+	for _, edge := range polygon_edges {
+		fmt.Printf("p1=(%d,%d), p2=(%d,%d)\n", edge.p1.y, edge.p1.x, edge.p2.y, edge.p2.x)
+	}
+
+	for i, row := range grid {
+		for j, col := range row {
+			node := Node{row: i, col: j, pipe: col}
+			if !slices.Contains(polygon_nodes, node) {
+				res := is_inside(node, grid, polygon_nodes)
+				if res {
+					output += 1
+					encloses = append(encloses, node)
+				}
+			}
+		}
+	}
+
+	for _, node := range encloses {
+		grid[node.row][node.col] = "I"
+	}
+
+	for _, row := range grid {
+		fmt.Println(row)
+	}
+
 	return output
 }
