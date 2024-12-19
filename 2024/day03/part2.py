4c3b3896b7a76190b5537a3e15cee27f39e1e885
Ismail Debbarh <usamdh07@gmail.com>
2024-12-19 18:25:23 +0100

    feat: Day03 part2 using python

---
+++ 2024/day03/part2.py
@@ -0,0 +1,74 @@
+input = open("real-input.txt","r").read()
+
+def solution():
+    l,r = 0,0
+    res = 0
+    do = True
+
+    while r < len(input):
+        current = input[l:r+1]
+
+        if current == "mul":
+            start_index = r+1
+            open = input[start_index]
+            start_index+=1
+            n1="" 
+
+            while input[start_index].isdigit(): 
+                n1+=input[start_index]
+                start_index+=1
+
+            s = input[start_index]
+            start_index+=1
+            n2= ""
+
+            while input[start_index].isdigit(): 
+                n2+=input[start_index]
+                start_index+=1
+
+            close = input[start_index]
+
+            if open == "(" and close == ")" and s == ",":
+                if do :
+                    res+=(int(n1)*int(n2))
+
+            r+=1
+            l=r
+        elif current in "mul":
+            r+=1
+        elif current == "do()":
+            do = True 
+            r+=1
+            l=r
+        elif current in "do()":
+            r+=1
+        elif current == "don't()":
+            do = False 
+            r+=1
+            l=r
+        elif current in "don't()":
+            r+=1
+        else:
+            l+=1
+            if l >= r:
+                r=l
+
+    return res
+
+print("solution: ",solution())
+
+
+
+
+
+
+    
+    
+
+
+
+
+
+
+
+
